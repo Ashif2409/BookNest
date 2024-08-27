5ffee981dc9a6b7952863a420d730769e3bcad6e
khalilul-dev <khalilul.rahman@alcinius.com>
2024-08-27 14:49:02 +0530

    f

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const main=require('./db/connection')
+const app = express();
+const port =process.env.PORT || 8000;
+
+// middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database Connection
+main().catch(err => console.log(err));
+
+//routes
+const userRoutes=require('./Routes/Users.routes');
+const bookRoutes=require('./Routes/Book.routes')
+const adminRoutes=require('./Routes/Admin.routes');
+const dueDateCheck = require('./Service/node-cron');
+
+dueDateCheck()
+app.use('/user',userRoutes);
+app.use('/book',bookRoutes);
+app.use('/admin',adminRoutes)
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
